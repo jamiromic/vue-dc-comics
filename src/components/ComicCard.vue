0eafd124e5a3db9e62ca6ab69e4b8af784fd609b
jamiromic <jamiromic@gmail.com>
2022-09-06 14:57:13 +0200

    list_cards_done!

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card">
+        <div class="picture">
+            <img :src="src" :alt="titolo">
+       </div>
+        <h4>{{ titolo }}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            src: String,
+            titolo: String,
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .card {
+
+        gap: 1rem;
+        width: 195px;
+        padding-bottom: 2rem;
+        
+
+        
+
+        .picture{
+
+            width: 180px;
+            height: 180px;
+            overflow: hidden;
+            
+        }
+
+        h4 {
+            color: rgba(255, 255, 255, 0.829);
+            text-transform: uppercase;
+            font-size: 0.74rem;
+            font-weight: 200;
+            padding-top: 1rem;
+        }
+
+        
+    }
+
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,12 +1,28 @@
 <template>
     <div class="container">
-        <a href="#">Content goes here</a>
+        <div class="wrapper">
+            <ComicCard v-for="(card,i) in dataCard" :key="i" :src="card.thumb" :titolo="card.series" />
+        </div>
+        <button>LOAD MORE</button>
+        
 
     </div>
 </template>
 
 <script>
+    import dataCard from '../dataCard'
+    import ComicCard from './ComicCard.vue'
+
+
     export default {
+        components: {
+            ComicCard
+        },
+        data() {
+            return {
+                dataCard: dataCard
+            }
+        }
         
     }
 </script>
@@ -15,12 +31,33 @@
 
     .container {
         background-color: #1C1C1C;
-        color: white;
         display: flex;
+        flex-direction: column;
         align-items: center;
-        padding: 3.5rem 16rem;
-        margin: 0 auto;
-        font-size: 2rem;
+        padding-bottom: 1.75rem;
+        
+
+        .wrapper {
+
+            color: white;
+            max-width: 1500px;
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            gap: 1rem;
+            padding: 3.5rem;
+            margin: 0 auto;
+            
+        }
+
+        button {
+            width: fit-content;
+            background-color:  #0282F9;
+            color: white;
+            padding: 0.75rem 4rem;
+            font-weight: 700;
+
+        }
     }
 
 </style>
